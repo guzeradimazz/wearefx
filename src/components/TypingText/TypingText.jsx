0e4d3408dd56d238a97793f52d794fafb4440b2e
Дима Гузерчук <dimaguzercuk@MacBook-Pro-Dima.local>
2021-12-04 23:24:42 +0300

    type

--- src/components/TypingText/TypingText.jsx
+++ src/components/TypingText/TypingText.jsx
@@ -1,31 +1,31 @@
-import React,{ useEffect,useRef } from 'react';
-import Typed from 'typed.js';
+import React, { useEffect, useRef } from 'react'
+import Typed from 'typed.js'
 import './TypingText.css'
 
 const TypingText = () => {
-  const element = useRef(null)
+    const element = useRef(null)
 
-  useEffect(() => {
-    const typed = new Typed (element.current,{
-      strings: ["Welcome to WeareFX, creative studio with big ideas",
-      "Where beauty and function are crafted together",
-      "Working across all media channels & platforms"
-    ],
-      startDelay: 300,
-      typeSpeed: 10,
-      backSpeed: 10,
-      backDelay: 900,
-      loop:true
-    });
-    return()=>typed.destroy()
-  }, [])
-
+    useEffect(() => {
+        const typed = new Typed(element.current, {
+            strings: [
+                'Welcome to WeareFX, creative studio with big ideas',
+                'Where beauty and function are crafted together',
+                'Working across all media channels & platforms',
+            ],
+            startDelay: 300,
+            typeSpeed: 30,
+            backSpeed: 10,
+            backDelay: 900,
+            loop: true,
+        })
+        return () => typed.destroy()
+    }, [])
 
     return (
-        <div className='typing__text'>
-          <span className='typing__text' ref={element}></span>
+        <div className="typing__text">
+            <span className="typing__text" ref={element}></span>
         </div>
-    );
-};
+    )
+}
 
-export default TypingText;
+export default TypingText
