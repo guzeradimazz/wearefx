7a9b9715deac4ccaedb65e777078c8b098849e58
Дима Гузерчук <dimaguzercuk@MacBook-Pro-Dima.local>
2021-12-12 15:23:19 +0300

    slider

--- src/components/MainPage/Swiper/SwiperSLider.jsx
+++ src/components/MainPage/Swiper/SwiperSLider.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import Swiper from 'react-id-swiper'
+import React, { Component } from 'react'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import SwiperItem from './SwiperItem';
 
-const SwiperSLider = () => {
-    const params = {
-        spaceBetween: 0,
-        grabCursor: true,
-        slidesPerView: 1,
-        centeredSlides: true,
+export default class SimpleSlider extends Component {
+    render() {
+        const settings = {
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
+        return (
+            <div>
+                <Slider {...settings}>
+                    <SwiperItem title={'Puma Ukraine'} paragraph={'Granding, VFX, motion'} current={1}/>
+                    <SwiperItem title={'Puma Ukraine'} paragraph={'Granding, VFX, motion'} current={2}/>
+                    <SwiperItem title={'Puma Ukraine'} paragraph={'Granding, VFX, motion'} current={3}/>
+                    <SwiperItem title={'Puma Ukraine'} paragraph={'Granding, VFX, motion'} current={4}/>
+                    <SwiperItem title={'Puma Ukraine'} paragraph={'Granding, VFX, motion'} current={5}/>
+                    <SwiperItem title={'Puma Ukraine'} paragraph={'Granding, VFX, motion'} current={6}/>
+                </Slider>
+            </div>
+        )
     }
-
-    return (
-        <Swiper {...params} className="swiper__mobile">
-            <div className='swiper__slide'>Slide 1</div>
-            <div className='swiper__slide'>Slide 2</div>
-            <div className='swiper__slide'>Slide 3</div>
-            <div className='swiper__slide'>Slide 4</div>
-            <div className='swiper__slide'>Slide 5</div>
-        </Swiper>
-    )
 }
-
-export default SwiperSLider
