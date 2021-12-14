5d60deb3872413ac7e7c77bc66cb7503ec87a6ae
Дима Гузерчук <dimaguzercuk@MacBook-Pro-Dima.local>
2021-12-14 22:44:20 +0300

    video223

--- src/components/Pages/CasesPagePersonal/CasesPagePersonal.jsx
+++ src/components/Pages/CasesPagePersonal/CasesPagePersonal.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import CursorProvider from '../../CursorProvider/CursorProvider'
 import './CasesPagePersonal.css'
 import { PersonalHeader } from './PersonalHeader'
 import { VideoPersonal } from './Video/VideoPersonal'
 
 const CasesPagePersonal = () => {
+    useEffect(() => {
+        document.getElementById('overlay1').classList.add('displayNone')
+        document.getElementById('overlay2').classList.add('displayNone')
+        document.getElementById('overlay3').classList.add('displayNone')
+    }, [])
     return (
         <CursorProvider>
             <div className="personal-page">
