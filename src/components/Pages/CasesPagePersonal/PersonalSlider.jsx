04d0f192823d2df131869a4d8d9f71493678530b
Дима Гузерчук <dimaguzercuk@MacBook-Pro-Dima.local>
2021-12-15 16:47:46 +0300

    sdfgd

--- src/components/Pages/CasesPagePersonal/PersonalSlider.jsx
+++ src/components/Pages/CasesPagePersonal/PersonalSlider.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 
 export const PersonalSlider = () => {
+    const refSlider = useRef(null)
+
+    const nextSlide = () => {
+        refSlider.current.slickNext()
+    }
+    const prevSlide = () => {
+        refSlider.current.slickPrev()
+    }
+    const settings = {
+        infinite: true,
+        speed: 500,
+        swipeToSlide: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+    }
     return (
-        <div>
-            <Slider>
-                <div>1</div>
-
-                <div>1</div>
-
-                <div>1</div>
-
-                <div>1</div>
-                <div>1</div>
+        <div className="personalSlider">
+            <Slider ref={refSlider} className="personal-slider" {...settings}>
+                <div className="slide1"></div>
+                <div className="slide2"></div>
+                <div className="slide3"></div>
+                <div className="slide4"></div>
+                <div className="slide5"></div>
+                <div className="slide1"></div>
+                <div className="slide2"></div>
+                <div className="slide3"></div>
+                <div className="slide4"></div>
+                <div className="slide5"></div>
             </Slider>
+            <button className="prev-slider" onClick={() => prevSlide()} />
+            <button className="next-slider" onClick={() => nextSlide()} />
         </div>
     )
 }
