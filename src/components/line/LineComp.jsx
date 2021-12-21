365aa53dc2f2747f50a05b65c8fa636f4f68cf38
Дима Гузерчук <dimaguzercuk@MacBook-Pro-Dima.local>
2021-12-21 21:14:02 +0300

    jgkhjfd88

--- src/components/line/LineComp.jsx
+++ src/components/line/LineComp.jsx
@@ -1,52 +1,33 @@
 import React, { useEffect } from 'react'
 
-export default function LineComp({coords}) {
-
+export default function LineComp({ coords }) {
     const canvas = document.getElementById('polyline')
     let ctx = canvas.getContext('2d')
-    ctx.fillStyle = 'blue';
 
+    useEffect(() => {
+        ctx.strokeStyle = 'rgb(248, 255, 143)'
+        canvas.width = canvas.clientWidth
+        canvas.height = canvas.clientHeight
+    }, [])
 
     const getRandomInt = (min, max) => {
         return Math.floor(min + Math.random() * (max + 1 - min))
     }
 
-    const pointsCount = 5
+    let YStart = canvas.height * 0.38
 
-    canvas.width = canvas.clientWidth
-    canvas.height = canvas.clientHeight
-
-    let YStart = canvas.height*0.38
+    const dotList = [
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+    ]
     const a = { x: 0, y: YStart },
-        b = { x: coords.x, y: coords.y }
+        b = { x: coords.x - 10, y: coords.y }
 
-    const min = 5,
-        max = 15
+    ctx.beginPath()
+    ctx.moveTo(a.x, a.y)
+    ctx.lineTo(b.x, b.y)
+    ctx.stroke()
 
-    for (let i = 0; i < pointsCount; i += 2) {
-        const pointsClose = Math.abs(a.x - b.x) < 3 * max && Math.abs(a.y - b.y) < 3 * max
-        if (pointsClose) {
-            ctx.beginPath()
-            ctx.moveTo(a.x, a.y)
-            ctx.lineTo(b.x, b.y)
-            ctx.stroke()
-            break
-        }
-        
-
-        ctx.beginPath()
-        ctx.moveTo(a.x, a.y)
-        a.x += getRandomInt(min, max)
-        a.y += getRandomInt(min, max)
-        ctx.lineTo(a.x, a.y)
-        ctx.stroke()
-
-        ctx.beginPath()
-        ctx.moveTo(b.x, b.y)
-        b.x -= getRandomInt(min, max)
-        b.y -= getRandomInt(min, max)
-        ctx.lineTo(b.x, b.y)
-        ctx.stroke()
-    }
     return <div></div>
 }
