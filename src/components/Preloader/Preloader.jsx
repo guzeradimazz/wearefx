c0473173f7f331779f9680e213be24dbe5af3c1c
Дима Гузерчук <dimaguzercuk@MacBook-Pro-Dima.local>
2022-01-19 00:36:37 +0300

    77777777yy7y

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -5,7 +5,7 @@ import preloader from './X_A2.mp4'
 const Preloader = ({isLoading}) => {
     return (
         <div className={isLoading ?``:'displayNone'}>
-            <video className="preloader" autoPlay loop muted>
+            <video className="preloader" autoPlay muted>
                 <source src={preloader} type="video/mp4" />
             </video>
         </div>
